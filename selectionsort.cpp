9ecdaef03e705376225b3dc2c7d5d980f7278fef
Ashish Job <114624617+Ashishjob@users.noreply.github.com>
2023-09-16 02:01:04 -0500

    feat: filled with code

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+void selectionSort(int a[], int n) {
+  int swaps;
+  int first = a[0];
+  int min = a[1];
+  for (int i = 0; i < n; i++) {
+    if (a[i] <= min) {
+      swap(min, a[i]);
+      min = a[i];
+      swaps++;
+    }
+  }
+}
