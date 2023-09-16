26c7b563143e64d30f48d040ad8cde66c418029b
Ashish Job <114624617+Ashishjob@users.noreply.github.com>
2023-09-16 02:00:04 -0500

    feat: filled with code

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
+
+void bubbleSort(int a[], int n) {
+  int temp, comp, swaps;
+  for (int pass = 1; pass < n; pass++) { // outer loop has n iterations
+    for (int i = 0; i < n - pass;
+         i++) { // inner keeps iterating until n - pass = 0
+      comp++;
+      if (a[i] > a[i + 1]) {
+        swaps++;
+        temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+      }
+    }
+  }
+  cout << "Bubble Sort: " << endl;
+  cout << "# of Comparisons: " << comp <<endl;
+  cout << "# of Swaps: " << swaps << endl;
+}
