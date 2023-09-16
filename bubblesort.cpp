a87c5721aed3877a8b2336be9bf923bb7475db98
Ashish Job <114624617+Ashishjob@users.noreply.github.com>
2023-09-16 02:04:40 -0500

    refactor: add explaining comments

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -4,16 +4,26 @@
 using namespace std;
 
 void bubbleSort(int a[], int n) {
-  int temp, comp, swaps;
-  for (int pass = 1; pass < n; pass++) { // outer loop has n iterations
-    for (int i = 0; i < n - pass;
-         i++) { // inner keeps iterating until n - pass = 0
-      comp++;
+  int temp, comp = 0, swaps = 0; // Initialize temporary variables, comparison count, and swap count.
+  
+  for (int pass = 1; pass < n; pass++) {
+    // The outer loop controls the number of passes through the array.
+    // In each pass, the largest unsorted element 'bubbles up' to its correct position at the end of the array.
+
+    for (int i = 0; i < n - pass; i++) {
+      // The inner loop iterates through the unsorted portion of the array.
+      // It compares adjacent elements and swaps them if they are in the wrong order.
+
+      comp++; // Increment the comparison count for each comparison made.
+
       if (a[i] > a[i + 1]) {
-        swaps++;
-        temp = a[i];
-        a[i] = a[i + 1];
-        a[i + 1] = temp;
+        // If the current element is greater than the next element, a swap is needed.
+
+        swaps++; // Increment the swap count for each swap made.
+
+        temp = a[i]; // Store the current element in a temporary variable.
+        a[i] = a[i + 1]; // Swap the current element with the next element.
+        a[i + 1] = temp; // Place the larger element in the next position.
       }
     }
   }
