54c0056fcc4c5fb53bbeff74dbb162128300d8ea
Ashish Job <114624617+Ashishjob@users.noreply.github.com>
2023-09-16 02:02:52 -0500

    feat: filled with code

--- bubblesortV2.cpp
+++ bubblesortV2.cpp
@@ -1,1 +1,24 @@
+void bubblesortv2(int a[], int n) {
+  int temp, comp = 0, swaps = 0;
+  bool swapped;
 
+  for (int pass = 1; pass < n; pass++) {
+    swapped = false; // Initialize the flag for this pass
+
+    for (int i = 0; i < n - pass; i++) {
+      comp++;
+      if (a[i] > a[i + 1]) {
+        swaps++;
+        temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+        swapped = true; // Set the flag to true if a swap occurs
+      }
+    }
+
+    // If no swaps were made during this pass, the array is already sorted
+    if (!swapped) {
+      break;
+    }
+  }
+}
